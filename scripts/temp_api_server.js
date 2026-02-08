2dc9a9c830b6a0627144b09136c56c5540fe5fa7
jayanthan-prog <jayanthan212@gmail.com>
2026-02-08 21:25:45 +0530

    token security update

--- scripts/temp_api_server.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-require('dotenv').config();
-const app = require('../src/api');
-const port = process.env.TEMP_PORT || 3002;
-app.listen(port, () => console.log(`TEMP_API_UP_${port}`));
